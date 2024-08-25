ce52424cd5539fc6f230a8f65850b692c07e8c1d
Rayssa Nunes <rayssa_nunes@outlook.com>
2024-08-25 20:16:16 -0300

    Alterando a conexão do banco de dados e atualização do .gitignore

--- ddl.py
+++ ddl.py
@@ -1,9 +1,16 @@
-import sqlite3
-from Globals import DATABASE_NAME
+import psycopg2
+import os
 
-connection = sqlite3.connect(DATABASE_NAME)
+connection = psycopg2.connect(
+    dbname = 'cafeapi',
+    user = os.environ['DB_USERNAME'],
+    password = os.environ['DB_PASSWORD'],
+    host = "localhost"
+)
 
 with open('schema.sql') as f:
-    connection.executescript(f.read())
-
+    with connection.cursor() as cursor:
+        cursor.execute(f.read())
+        
+connection.commit()
 connection.close()
--- helpers/database/__init__.py
+++ helpers/database/__init__.py
@@ -1,13 +1,13 @@
-import sqlite3
-from Globals import DATABASE_NAME
+import psycopg2
+import os
 
 
 def get_db_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect(DATABASE_NAME)
-        conn.row_factory = sqlite3.Row
-    except sqlite3.Error as e:
-        print('Não foi possível conectar')
-
+    conn = psycopg2.connect(
+        host = "localhost",
+        database = "cafeapi",
+        user = os.environ['DB_USERNAME'],
+        password = os.environ['DB_PASSWORD'])
+    
     return conn
+
