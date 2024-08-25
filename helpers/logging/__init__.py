ab10c79d8cac1ada183babc137931a73aadc92cd
Rayssa Nunes <rayssa_nunes@outlook.com>
2024-08-25 20:50:09 -0300

    Deleção do arquivo helloflask e atualização do arquivo de inicialização do logging

--- helpers/logging/__init__.py
+++ helpers/logging/__init__.py
@@ -4,14 +4,9 @@ import logging
 # Logging
 logger = logging
 
-# fileHandler = logging.FileHandler("helloflask.log", mode='w')
-fileHandler = logging.RotatingFileHandler(
-    "my_log.log", maxBytes=2000, backupCount=10)
-consoleHandler = logging.StreamHandler()
-
 logger.basicConfig(level=logging.INFO,
                    format='%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s',
-                   handlers=[fileHandler, consoleHandler])
+                   handlers=[logging.FileHandler("helloflask.log", mode='w'), logging.StreamHandler()])
 stream_handler = [h for h in logger.root.handlers if isinstance(
     h, logger.StreamHandler)][0]
 stream_handler.setLevel(logger.INFO)
